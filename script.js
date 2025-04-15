47b7ad7257e34c1596229f24ccb61400e499f018
Denys Kuzin <denis.kuzin98213@gmail.com>
2025-04-15 16:21:59 +0200

    my result lesson 2

--- script.js
+++ script.js
@@ -8,18 +8,37 @@ const personalMovieDB = {
   privat: false,
 };
 
-const lastFilm = prompt("What one of last film did you watch?", "");
-const assessmentLastFilm = prompt(
-  "What do you give assessment for that film from 0 to 10?",
-  ""
-);
-const lastFilm2 = prompt("What one of last film did you watch?", "");
-const assessmentLastFilm2 = prompt(
-  "What do you give assessment for that film from 0 to 10?",
-  ""
-);
+const countQuest = 2;
 
-personalMovieDB.movies[lastFilm] = assessmentLastFilm;
-personalMovieDB.movies[lastFilm2] = assessmentLastFilm2;
+for (let i = 1; i <= countQuest; i++) {
+
+  const lastFilm = prompt("What one of last film did you watch?", "");
+
+  if (lastFilm === null || lastFilm === "" || lastFilm.length > 50) {
+    alert("Please enter a valid film name (not empty and < 50 characters).");
+    i--;
+    continue;
+  }
+
+  const assessmentLastFilm = prompt(
+    "Give assessment for that film from 0 to 10?",
+    ""
+  );
+
+  if (
+    assessmentLastFilm === null ||
+    assessmentLastFilm === "" ||
+    assessmentLastFilm >= 10 ||
+    assessmentLastFilm <= 0
+  ) {
+    alert("Please enter assesment like number from 0 to 10");
+    i--;
+    continue;
+  }
+
+  personalMovieDB.movies[lastFilm] = assessmentLastFilm;
+}
+
+
 
 console.log(personalMovieDB);
